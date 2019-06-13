359a436740997f6825242f7ac2a7fbc244f37788
shalev.goldfarb <goldensulv@gmail.com>
2019-06-13 19:11:44 +0300

    initial commit

---
+++ SpaceChuckie.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+void say_hello(){
+    std::cout << "Hello, from SpaceChuckie!\n";
+}
+
+int main(void)
+{
+    say_hello();
+
+    return 0;
+}
---
+++ build/CMakeFiles/SpaceChuckie.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SpaceChuckie.dir/SpaceChuckie.cpp.o"
+  "libSpaceChuckie.pdb"
+  "libSpaceChuckie.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SpaceChuckie.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
