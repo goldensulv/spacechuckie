b90de8add5ca3b7ec2e0b9b9d137334094d0c74d
Shalev <goldensulv@gmail.com>
2019-07-01 13:57:28 +0300

    Just some changes

--- tutorial-01/main.cpp
+++ tutorial-01/main.cpp
@@ -5,6 +5,8 @@
 #include "Game.h"
 #include "Texture.h"
 
+static const int s_RESIZE = 5;
+
 int main(int argc, char* argv[])
 {
     Game game;
@@ -14,6 +16,20 @@ int main(int argc, char* argv[])
 
     Texture texture;
     texture.loadFromFile("astronaut_walking_grey.png", game.getRenderer());
+
+    int w_t = texture.getWidth();
+    int h_t = texture.getHeight();
+
+    int w_g = game.getWidth();
+    int h_g = game.getHeight();
+
+    SDL_Renderer* renderer = game.getRenderer();
+
+    Texture background;
+    texture.loadFromFile("background.png", renderer);
+
+    game.render(background, 0, 0, w_g, h_g);
+
     while (game.isRunning())
     {
         SDL_Event event;
@@ -26,9 +42,9 @@ int main(int argc, char* argv[])
             else
             {
                 SDL_RenderClear(game.getRenderer());
-                std::cout << "width: " << texture.getWidth() << std::endl << "height: " << texture.getHeight() << std::endl;
-                game.render(texture, game.getWidth() / 2 - texture.getWidth() / 10, game.getHeight() - texture.getHeight() / 10, texture.getWidth() / 10, texture.getHeight() / 10);
-                SDL_RenderPresent(game.getRenderer());
+                std::cout << "width: " << w_t << std::endl << "height: " << h_t << std::endl;
+                game.render(texture, w_g / 2 - w_t / s_RESIZE, h_g - h_t / s_RESIZE, w_t / s_RESIZE, h_t / s_RESIZE);
+                SDL_RenderPresent(renderer);
             }
         }
     }
